0131179bc85d9548ddb1b9db07f5d24eff8ee457
sandeepenrique <sandi10.96@gmail.com>
2017-04-22 20:48:02 +0530

    update customer.java

--- src/main/java/com/funcart/customer/Customer.java
+++ src/main/java/com/funcart/customer/Customer.java
@@ -3,8 +3,36 @@ package com.funcart.customer;
 public class Customer {
 	private String username;
 	private String password;
+	private long phoneNumber;
+	private String shippingAddress;
+	private String billingAddress;
 	
+	//getter setter
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+	public String getBillingAddress() {
+		return billingAddress;
+	}
+
+	public void setBillingAddress(String billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+
 	private String email;
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
 	public String getUsername() {
 		return username;
@@ -28,5 +56,21 @@ public class Customer {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	//constructor
+	public Customer(String username, String password, long phoneNumber, String shippingAddress, String billingAddress,
+			String email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+		this.shippingAddress = shippingAddress;
+		this.billingAddress = billingAddress;
+		this.email = email;
+	}
+
+	public Customer() {
+		super();
 	}
 }
